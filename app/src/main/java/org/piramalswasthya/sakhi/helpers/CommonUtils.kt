9388eec08dc895d7ed36d91308b8f77bdf9fc62f
ADevBee <ramanan.g@beehyv.com>
2023-04-13 18:44:51 +0530

    changed the record count logic by adding a single RecordsRepo.kt singleton to save all flows from database changed from LiveData.. forgot to add CommonUtils.kt

--- app/src/main/java/org/piramalswasthya/sakhi/helpers/CommonUtils.kt
+++ app/src/main/java/org/piramalswasthya/sakhi/helpers/CommonUtils.kt
@@ -9,20 +9,25 @@ fun filterBenList(list: List<BenBasicDomain> ,text: String): List<BenBasicDomain
     else{
         val filterText = text.lowercase()
         return list.filter {
-            it.hhId.toString().lowercase().contains(filterText) ||
-                    it.benId.toString().lowercase().contains(filterText) ||
-                    it.regDate.lowercase().contains((filterText)) ||
-                    it.age.lowercase().contains(filterText) ||
-                    it.benName.lowercase().contains(filterText) ||
-                    it.familyHeadName.lowercase().contains(filterText) ||
-                    it.benSurname?.lowercase()?.contains(filterText) ?: false ||
-                    it.typeOfList.lowercase().contains(filterText) ||
-                    it.mobileNo.lowercase().contains(filterText) ||
-                    it.gender.lowercase().contains(filterText) ||
-                    it.fatherName?.lowercase()?.contains(filterText) ?: false
+            filterForBen(it, filterText)
         }
     }
 }
+ fun filterForBen(
+    ben: BenBasicDomain,
+    filterText: String
+) = ben.hhId.toString().lowercase().contains(filterText) ||
+        ben.benId.toString().lowercase().contains(filterText) ||
+        ben.regDate.lowercase().contains((filterText)) ||
+        ben.age.lowercase().contains(filterText) ||
+        ben.benName.lowercase().contains(filterText) ||
+        ben.familyHeadName.lowercase().contains(filterText) ||
+        ben.benSurname?.lowercase()?.contains(filterText) ?: false ||
+        ben.typeOfList.lowercase().contains(filterText) ||
+        ben.mobileNo.lowercase().contains(filterText) ||
+        ben.gender.lowercase().contains(filterText) ||
+        ben.fatherName?.lowercase()?.contains(filterText) ?: false
+
 @JvmName("filterBenList1")
 fun filterBenList(list: List<BenBasicDomainForForm>, text: String): List<BenBasicDomainForForm> {
     if (text == "")
