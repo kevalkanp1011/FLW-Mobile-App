a4367328b490cef6f837c0c4380edb97959fa054
Sushant <sushant.yadav@bizbrolly.com>
2024-09-17 16:23:04 +0530

    Added Comments
    # Conflicts:
    #	app/src/main/java/org/piramalswasthya/sakhi/network/AbhaApiService.kt
    #	app/src/main/java/org/piramalswasthya/sakhi/repositories/AbhaIdRepo.kt

--- app/src/main/java/org/piramalswasthya/sakhi/network/AbhaApiService.kt
+++ app/src/main/java/org/piramalswasthya/sakhi/network/AbhaApiService.kt
@@ -1,30 +1,49 @@
 package org.piramalswasthya.sakhi.network
 
 import okhttp3.ResponseBody
-import org.piramalswasthya.sakhi.BuildConfig
 import retrofit2.Response
 import retrofit2.http.*
 
 interface AbhaApiService {
 
+    // Staging (v1/v2/v3)
     @Headers("No-Auth: true")
     @POST
     suspend fun getToken(
-        @Url url: String = BuildConfig.ABHA_TOKEN_URL,
+        @Url url: String = "https://dev.abdm.gov.in/gateway/v0.5/sessions",
         @Body request: AbhaTokenRequest = AbhaTokenRequest()
     ): Response<ResponseBody>
 
+    // Production (v1/v2/v3)
+//    @Headers("No-Auth: true")
+//    @POST
+//    suspend fun getToken(
+//        @Url url: String = "https://live.abdm.gov.in/gateway/v0.5/sessions",
+//        @Body request: AbhaTokenRequest = AbhaTokenRequest()
+//    ): Response<ResponseBody>
+
+    // Generate OTP (v1)
     @POST("v1/registration/aadhaar/generateOtp")
     suspend fun generateAadhaarOtp(@Body aadhaar: AbhaGenerateAadhaarOtpRequest): Response<ResponseBody>
 
+    // Generate OTP (v2)
     @POST("v2/registration/aadhaar/generateOtp")
     suspend fun generateAadhaarOtpV2(@Body aadhaar: AbhaGenerateAadhaarOtpRequest): Response<ResponseBody>
+
+    // Generate OTP (v3)
+    @POST("v3/enrollment/request/otp")
+    suspend fun generateAadhaarOtpV3(@Body aadhaar: AbhaGenerateAadhaarOtpRequest, @Header("REQUEST-ID") requestId: String, @Header("TIMESTAMP") timestamp: String): Response<ResponseBody>
 
     @POST("v1/registration/aadhaar/resendAadhaarOtp")
     suspend fun resendAadhaarOtp(@Body aadhaar: AbhaResendAadhaarOtpRequest): Response<ResponseBody>
 
+    // Verify OTP (v1/v2)
     @POST("v1/registration/aadhaar/verifyOTP")
     suspend fun verifyAadhaarOtp(@Body request: AbhaVerifyAadhaarOtpRequest): Response<ResponseBody>
+
+    // Verify OTP (v3)
+    @POST("v3/enrollment/enrol/byAadhaar")
+    suspend fun verifyAadhaarOtp3(@Body request: AbhaVerifyAadhaarOtpRequest, @Header("REQUEST-ID") requestId: String, @Header("TIMESTAMP") timestamp: String): Response<ResponseBody>
 
     @POST("v1/registration/aadhaar/generateMobileOTP")
     suspend fun generateMobileOtp(@Body mobile: AbhaGenerateMobileOtpRequest): Response<ResponseBody>
@@ -34,6 +53,10 @@ interface AbhaApiService {
 
     @POST("v1/registration/aadhaar/verifyMobileOTP")
     suspend fun verifyMobileOtp(@Body request: AbhaVerifyMobileOtpRequest): Response<ResponseBody>
+
+    // Verify Updated Mobile OTP (v3)
+    @POST("v3/enrollment/auth/byAbdm")
+    suspend fun verifyMobileOtp3(@Body request: AbhaVerifyMobileOtpRequest, @Header("REQUEST-ID") requestId: String, @Header("TIMESTAMP") timestamp: String): Response<ResponseBody>
 
     @POST("v1/registration/aadhaar/createHealthIdWithPreVerified")
     suspend fun createAbhaId(@Body request: CreateAbhaIdRequest): Response<ResponseBody>
@@ -47,10 +70,23 @@ interface AbhaApiService {
     @GET("v1/account/getPngCard")
     suspend fun getPngCard(): Response<ResponseBody>
 
+    // Staging RES Encryption API (v1/v2)
+//    @GET
+//    suspend fun getAuthCert(
+//        @Url url: String = "https://healthidsbx.abdm.gov.in/api/v2/auth/cert"
+//    ): Response<ResponseBody>
+
+    // Staging RES Encryption API (v3)
     @GET
     suspend fun getAuthCert(
-        @Url url: String = BuildConfig.ABHA_AUTH_URL
+        @Url url: String = "https://healthidsbx.abdm.gov.in/api/v1/auth/cert"
     ): Response<ResponseBody>
+
+    // Production RES Encryption API (v3)
+//    @GET
+//    suspend fun getAuthCert(
+//        @Url url: String = "https://healthid.abdm.gov.in/api/v1/auth/cert"
+//    ): Response<ResponseBody>
 
     @GET("v2/ha/lgd/states")
     suspend fun getStateAndDistricts(): Response<ResponseBody>
