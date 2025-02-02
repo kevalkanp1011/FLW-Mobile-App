794c31cef81c6608b1aa16c78dfa4809c8579147
Keval Kanpariya <74661970+kevalkanp1011@users.noreply.github.com>
2025-02-02 22:52:13 +0530

    Update native-lib.cpp

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -112,8 +112,9 @@ extern "C"
 JNIEXPORT jstring JNICALL
 Java_org_piramalswasthya_sakhi_utils_KeyUtils_encryptedPassKey(JNIEnv *env, jobject thiz) {
     // Retrieve the Base64 encoded cipher text from the build-time define.
-    string encryptedBase64 = ENCRYPTED_PASS_KEY;
-    string decryptedStr = decryptBase64EncodedString(encryptedBase64);
+    // string encryptedBase64 = ENCRYPTED_PASS_KEY;
+    // string decryptedStr = decryptBase64EncodedString(encryptedBase64);
+    string decryptedStr = ENCRYPTED_PASS_KEY;
     return env->NewStringUTF(decryptedStr.c_str());
 }
 
@@ -122,8 +123,9 @@ Java_org_piramalswasthya_sakhi_utils_KeyUtils_encryptedPassKey(JNIEnv *env, jobj
 extern "C"
 JNIEXPORT jstring JNICALL
 Java_org_piramalswasthya_sakhi_utils_KeyUtils_abhaClientSecret(JNIEnv *env, jobject thiz) {
-    string encryptedBase64 = ABHA_CLIENT_SECRET;
-    string decryptedStr = decryptBase64EncodedString(encryptedBase64);
+    // string encryptedBase64 = ABHA_CLIENT_SECRET;
+    // string decryptedStr = decryptBase64EncodedString(encryptedBase64);
+    string decryptedStr = ABHA_CLIENT_SECRET;
     return env->NewStringUTF(decryptedStr.c_str());
 }
 
@@ -132,8 +134,9 @@ Java_org_piramalswasthya_sakhi_utils_KeyUtils_abhaClientSecret(JNIEnv *env, jobj
 extern "C"
 JNIEXPORT jstring JNICALL
 Java_org_piramalswasthya_sakhi_utils_KeyUtils_abhaClientID(JNIEnv *env, jobject thiz) {
-    string encryptedBase64 = ABHA_CLIENT_ID;
-    string decryptedStr = decryptBase64EncodedString(encryptedBase64);
+    // string encryptedBase64 = ABHA_CLIENT_ID;
+    // string decryptedStr = decryptBase64EncodedString(encryptedBase64);
+    string decryptedStr = ABHA_CLIENT_ID;
     return env->NewStringUTF(decryptedStr.c_str());
 }
 
@@ -142,8 +145,9 @@ Java_org_piramalswasthya_sakhi_utils_KeyUtils_abhaClientID(JNIEnv *env, jobject 
 extern "C"
 JNIEXPORT jstring JNICALL
 Java_org_piramalswasthya_sakhi_utils_KeyUtils_baseTMCUrl(JNIEnv *env, jobject thiz) {
-    string encryptedBase64 = BASE_TMC_URL;
-    string decryptedStr = decryptBase64EncodedString(encryptedBase64);
+    // string encryptedBase64 = BASE_TMC_URL;
+    // string decryptedStr = decryptBase64EncodedString(encryptedBase64);
+    string decryptedStr = BASE_TMC_URL;
     return env->NewStringUTF(decryptedStr.c_str());
 }
 
@@ -152,8 +156,9 @@ Java_org_piramalswasthya_sakhi_utils_KeyUtils_baseTMCUrl(JNIEnv *env, jobject th
 extern "C"
 JNIEXPORT jstring JNICALL
 Java_org_piramalswasthya_sakhi_utils_KeyUtils_baseAbhaUrl(JNIEnv *env, jobject thiz) {
-    string encryptedBase64 = BASE_ABHA_URL;
-    string decryptedStr = decryptBase64EncodedString(encryptedBase64);
+    // string encryptedBase64 = BASE_ABHA_URL;
+    // string decryptedStr = decryptBase64EncodedString(encryptedBase64);
+    string decryptedStr = BASE_ABHA_URL;
     return env->NewStringUTF(decryptedStr.c_str());
 }
 
@@ -162,8 +167,9 @@ Java_org_piramalswasthya_sakhi_utils_KeyUtils_baseAbhaUrl(JNIEnv *env, jobject t
 extern "C"
 JNIEXPORT jstring JNICALL
 Java_org_piramalswasthya_sakhi_utils_KeyUtils_abhaTokenUrl(JNIEnv *env, jobject thiz) {
-    string encryptedBase64 = ABHA_TOKEN_URL;
-    string decryptedStr = decryptBase64EncodedString(encryptedBase64);
+    // string encryptedBase64 = ABHA_TOKEN_URL;
+    // string decryptedStr = decryptBase64EncodedString(encryptedBase64);
+    string decryptedStr = ABHA_TOKEN_URL;
     return env->NewStringUTF(decryptedStr.c_str());
 }
 
@@ -172,7 +178,8 @@ Java_org_piramalswasthya_sakhi_utils_KeyUtils_abhaTokenUrl(JNIEnv *env, jobject 
 extern "C"
 JNIEXPORT jstring JNICALL
 Java_org_piramalswasthya_sakhi_utils_KeyUtils_abhaAuthUrl(JNIEnv *env, jobject thiz) {
-    string encryptedBase64 = ABHA_AUTH_URL;
-    string decryptedStr = decryptBase64EncodedString(encryptedBase64);
+    // string encryptedBase64 = ABHA_AUTH_URL;
+    // string decryptedStr = decryptBase64EncodedString(encryptedBase64);
+    string decryptedStr = ABHA_AUTH_URL;
     return env->NewStringUTF(decryptedStr.c_str());
 }
